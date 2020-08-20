fc6f9944e29e093bcca1be4bc05d96fe4d1c821d
xinyu <xinyu.olivier@epitech.eu>
2020-08-20 19:43:48 +0200

    get orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -27,7 +27,7 @@ namespace App\Http\Controllers;
 
         public function store(Request $requests)
         {
-            $orders = json_decode($requests->orders, true);
+            $orders = $requests->orders;
 
             dd($orders);
 /*
