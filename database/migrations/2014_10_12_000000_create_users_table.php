55144666b4d68657a53b84909a4d52d01aae6691
xinyu <xinyu.olivier@epitech.eu>
2020-08-20 23:08:27 +0200

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -38,7 +38,7 @@ class CreateUsersTable extends Migration
     public function down()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-        Schema::dropIfExists('Users');
+        Schema::dropIfExists('users');
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
