b155bb94899f5539500b9197e30b7f5b1ae45749
xinyu <xinyu.olivier@epitech.eu>
2020-08-20 22:24:31 +0200

    get orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -53,11 +53,11 @@ namespace App\Http\Controllers;
                     'quantity' => $order["quantity"],
                    // 'price' => $order->price,
                     ]);
-                dd(json([
+                dd([
                     'status' => (bool) $orderCreate,
                     'data'   => $orderCreate,
                     'message' => $orderCreate ? 'Order Created!' : 'Error Creating Order'
-                ]));
+                ]);
 /* 
                     return response()->json([
                         'status' => (bool) $orderCreate,
